9c83f4e2d62b8eeb958c017d558772af57328bab
ikusamura <i.kusamura@gmail.com>
2020-03-15 13:09:39 +0900

    fix icon

--- src/components/link.js
+++ src/components/link.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = (props) => {
   return (
-    <span class="fas fa-cloud"></span>
+    <i class="fas fa-cloud"></i>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './app.css';
-import 'font-awesome/scss/font-awesome.scss'
 
 ReactDOM.render(<App />, document.getElementById('app'));
