6a071199526bd14adc559a98e47242cdd99d3473
ikusamura <i.kusamura@gmail.com>
2020-03-15 15:56:58 +0900

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div id="footer">
       <div className="container">
         <p>Based on UptimeRobot API interface, detection frequency is 5 minutes</p>
-        <p>We provides performance and security to our clients via its intelligent global network. This is the system status for the Our IPLC/IEPL Services.</p>
+        <p>We provides performance and security to our clients via its intelligent global network. This is the system status for the Our IPLC&IEPL&Website Services.</p>
       </div>
     </div>
   );
