a378bccc979b8c767eb9a150b1c7ca844fda5599
ikusamura <i.kusamura@gmail.com>
2020-03-15 15:33:58 +0900

    fix icon

--- src/components/link.js
+++ src/components/link.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = (props) => {
   return (
-    <img src='./cloud.png' height="16" width="16"/>
+    <pre><img src='./cloud.png' height="16" width="16"/></pre>
   );
 }
 
